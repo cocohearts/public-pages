4a8c9d95e3690c333ab096f59893f13b507180cd
Alex Zhao <alexzhao.mailbox@gmail.com>
2024-08-18 01:00:21 +0800

    update js

--- docs/extra.js
+++ docs/extra.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const siteNameElements = document.querySelectorAll('.site-name');
     siteNameElements.forEach(function (element) {
         const link = document.createElement('a');
-        link.href = '/';
+        link.href = '/public-pages';
         link.innerHTML = element.innerHTML;
         link.style.textDecoration = 'none'; // Remove underline
         element.innerHTML = '';
